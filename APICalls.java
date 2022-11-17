0b8a7f093511f24dffcf820d546271c761d92ba7
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-11-17 16:59:08 -0500

    WIP on APICalls

--- APICalls.java
+++ APICalls.java
@@ -1,4 +1,11 @@
+import java.util.ArrayList;
+
+
 public class APICalls {
 
-    
+    Driver commandsArray = new Driver();
+    ArrayList<String> newCommands = Driver.commands;
+
+    System.out.print();
+
 }
--- Driver.java
+++ Driver.java
@@ -1,18 +1,9 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 import java.io.File;
 import java.io.IOException;
 
 public class Driver {
-
-    //gets stored in the form of ID value
-
-    public Integer[] ID = new Integer[]{}; //array to write to from input file for IDs
-
-    public Integer[] value = new Integer[]{}; //array to write to from input file for values
-
     public static ArrayList<String> commands = new ArrayList<String>();
 
 
@@ -28,14 +19,12 @@ public class Driver {
 
         while(reader.hasNext()){
             commands.add(reader.nextLine());
-
-//            for(int i=0;i<numOfInputProcesses;i++){
-//                String processName = "Process " + i;
-//                int readyTime= reader.nextInt();
-//                int processingTime = reader.nextInt();
-//                processes.add(new UserProcess(userName, processName, readyTime, processingTime, "Waiting", false));
-            }
+        }
         System.out.print(commands);
         }
+
+    public ArrayList<String> getCommands() {
+        return commands;
+    }
     }
 
