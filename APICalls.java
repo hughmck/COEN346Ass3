63d8a31dff705db8795eb48b44cb2c98e0f47124
hugh mckenzie <64859308+hughmck@users.noreply.github.com>
2022-11-25 11:47:23 -0500

    WIP

--- APICalls.java
+++ APICalls.java
@@ -1,11 +1,70 @@
 import java.util.ArrayList;
 
-
 public class APICalls {
 
-    Driver commandsArray = new Driver();
-    ArrayList<String> newCommands = Driver.commands;
 
-    System.out.print();
+    private ArrayList<Commands> commandCount = new ArrayList<Commands>();
+    private String command;
+    private Integer variableID;
+    private Integer value;
+    private int commandCount;
 
-}
+
+    public Commands(Character command, Integer variableID, Integer value){
+        this.command = command;
+        this.variableID = variableID;
+        this.value = value;
+    }
+
+    public Commander(char command, int var) {
+        this.command = command;
+        this.variableID = var;
+    }
+
+
+    //number of commands
+    public int seeCommandCount(){
+        return commandCount;
+    }
+
+    public Commander(ArrayList<Character> orders, ArrayList<Integer> vars, ArrayList<Integer> vals){
+        int valcount =0;
+
+
+        for(int i=0; i< orders.size(); i++){
+            if(orders.get(i).equals('s')){
+                commandList.add(new Commander('s', vars.get(i), vals.get(valcount++)));
+            } else if (orders.get(i).equals('l')){
+                commandList.add(new Commander('l', vars.get(i)));
+            }else{
+                commandList.add(new Commander('r', vars.get(i)));
+            }
+        }
+        commandCount = commandList.size();
+    }
+
+
+    //check next command to run
+    public synchronized Commander nextCommand(){
+        Commander temp = new Commander(commandList.get(0).command, commandList.get(0).var, commandList.get(0).value);
+        commandList.remove(0);
+        commandCount--;
+        return temp;
+
+    }
+
+
+    public Character getCommand() {
+        return command;
+    }
+
+    public Integer getVar() {
+        return variableID;
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+
+    }
--- Driver.java
+++ Driver.java
@@ -4,8 +4,15 @@ import java.io.File;
 import java.io.IOException;
 
 public class Driver {
-    public static ArrayList<String> commands = new ArrayList<String>();
+    public static ArrayList<APICalls> commandsAPICalls = new ArrayList<APICalls>();
 
+    ArrayList<String> commands = new ArrayList<String>(); //receives the string of the command
+    ArrayList<Integer> variableID = new ArrayList<Integer>(); //returns the variableID of the command
+    ArrayList<Integer> value = new ArrayList<Integer>(); //returns the value stored in the command
+
+    APICalls apiCalls;
+
+    apiCalls = new Commands(commands, variableID, value);
 
     public static void main(String[] args){
         Scanner reader = null;
@@ -20,11 +27,10 @@ public class Driver {
         while(reader.hasNext()){
             commands.add(reader.nextLine());
         }
-        System.out.print(commands);
+        for(int i = 0; i < commands.size(); i++) {
+            System.out.print(commands.get(i));
+            System.out.print(", ");
         }
-
-    public ArrayList<String> getCommands() {
-        return commands;
     }
-    }
+}
 
